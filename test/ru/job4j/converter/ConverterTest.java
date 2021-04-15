de195f3e304d91c3382653dbbbb0306ac2a55e6d
AMakutsevi4 <Qwerty123>
2021-04-15 15:05:10 +0900

    1. Модульные тесты. [#487710]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert400RblThen5Euro() {
+        int in = 400;
+        int expected = 5;
+        int out = Converter.rubleTOeuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dollars() {
+        int in = 180;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected,out);
+
+    }
+}
