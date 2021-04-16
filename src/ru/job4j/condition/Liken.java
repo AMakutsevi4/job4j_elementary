edfa405c94975c9713b1cad10648dab226863d3f
AMakutsevi4 <Qwerty123>
2021-04-16 10:05:22 +0900

    0. Операции сравнения [#487625]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Liken {
+
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean resultMore = first > second;
+        System.out.println(resultMore);
+
+        boolean resultLess = first < second;
+        System.out.println(resultLess);
+
+        boolean resultEquals = first == second;
+        System.out.println(resultEquals);
+    }
+}
