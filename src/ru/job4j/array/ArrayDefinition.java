cd62f9a3270bd9e420b315c5e91f4ccefb7b9267
AMakutsevi4 <Qwerty123>
2021-04-22 11:24:22 +0900

    6.0.2. Заполнение массива. [#487658]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Alehander Makutsevich";
+        names[1] = "Sergey Merkuriev";
+        names[2] = "Petr Arsentev";
+        names[3] = "Barack Obama";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
     }
 }
