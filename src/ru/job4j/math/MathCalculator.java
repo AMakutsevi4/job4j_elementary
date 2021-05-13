05d528a134c18e4db2ef73288f0e8f4e2fbe916a
AMakutsevi4 <Qwerty123>
2021-05-13 15:40:09 +0900

    1.1. Что такое import [#504773]

--- src/ru/job4j/math/MathCalculator.java
+++ src/ru/job4j/math/MathCalculator.java
@@ -1,13 +1,16 @@
 package ru.job4j.math;
 
+import static ru.job4j.math.MathFunction.*;
+
 public class MathCalculator {
 
     public static double amountTwoMethod(double first, double second) {
-        return MathFunction.difference(first, second) + MathFunction.share(first, second);
+        return difference(first, second) + share(first, second);
     }
 
     public static double amountFourMethod(double first, double second) {
-        return MathFunction.sum(first, second) + MathFunction.difference(first, second) + MathFunction.share(first, second) + MathFunction.multiply(first, second);
+        return sum(first, second) + difference(first, second) +
+                share(first, second) + multiply(first, second);
     }
 
     public static void main(String[] args) {
